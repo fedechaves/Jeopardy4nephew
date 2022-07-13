e0ca5bb386fcb93b3f223023965cc470f54ce3da
fedechaves <federicochavesf@gmail.com>
2022-07-12 20:02:54 -0400

    setting APIs for categories

--- main.js
+++ main.js
@@ -38,4 +38,44 @@ function getClue(){
     console.log('have a nice day')
 }
 
-// API
+// CALL API
+
+//random function
+function randInt(){
+    return Math.floor(Math.random() * (18418) + 1)
+}
+
+//change scope of array of category API
+let catArray = []
+
+function buildCategories() {
+
+    const fetchReq1 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq2 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq3 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq4 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq5 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq6 = fetch(`https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const allData = Promise.all([fetchReq1, fetchReq2, fetchReq3, fetchReq4, fetchReq5, fetchReq6])
+
+    allData.then((res)=> {
+        console.log(res)
+        catArray = res
+    })
+}
+
+function setCategories(catArray) {
+    let element
+}
